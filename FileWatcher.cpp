93ce4dd1e4c699bb6d57425e1261bde0f099303f
Arka Seth <arkaseth@gmail.com>
2025-11-23 19:31:32 +0530

    final

--- FileWatcher.cpp
+++ FileWatcher.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
 #include "FileWatcher.hpp"
+
+int main()
+{
+    FileWatcher fw("./", std::chrono::milliseconds(5000));
+
+    fw.start([](std::string path, FileStatus status) -> void
+             {
+        if (!std::filesystem::is_regular_file(std::filesystem::path(path)) && status != FileStatus::erased) {
+            return;
+        }
+
+        switch (status) {
+            case FileStatus::created:
+                std::cout << "File created: " << path << "\n";
+                break;
+            case FileStatus::modified:
+                std::cout << "File modified: " << path << "\n";
+                break;
+            case FileStatus::erased:
+                std::cout << "File erased: " << path << "\n";
+                break;
+            default:
+                std::cout << "Unknown file status.\n";
+        } });
+}
--- FileWatcher.hpp
+++ FileWatcher.hpp
@@ -7,21 +7,75 @@
 #include <string>
 #include <functional>
 
-enum class FileStatus { created, modified, erased };
+enum class FileStatus
+{
+    created,
+    modified,
+    erased
+};
 
-class FileWatcher {
+class FileWatcher
+{
 public:
     std::string path;
-    // Time interval for checking base folder for changes
+    // Time interval in milliseconds for checking base folder for changes
     std::chrono::duration<int, std::milli> delay;
 
     // Record of files from base dir and their last modified time
-    FileWatcher(std::string path, std::chrono::duration<int, std::milli> delay) : path(path), delay(delay) {
-        for (auto &file : std::filesystem::recursive_directory_iterator(path)) {
+    FileWatcher(std::string path, std::chrono::duration<int, std::milli> delay) : path(path), delay(delay)
+    {
+        for (auto &file : std::filesystem::recursive_directory_iterator(path))
+        {
             paths_[file.path().string()] = std::filesystem::last_write_time(file);
+        }
+    }
+
+    // Monitor path for changes, and execute user-supplied action in case of a change
+    void start(const std::function<void(std::string, FileStatus)> &action)
+    {
+        while (running_)
+        {
+            // Wait for "delay"
+            std::this_thread::sleep_for(delay);
+
+            // Check if file was deleted
+            auto it = paths_.begin();
+            while (it != paths_.end())
+            {
+                if (!std::filesystem::exists(it->first))
+                {
+                    action(it->first, FileStatus::erased);
+                    it = paths_.erase(it);
+                }
+                else
+                    it++;
+            }
+
+            // Check if file was modified or created
+            for (auto &file : std::filesystem::recursive_directory_iterator(path))
+            {
+                auto current_file_last_write_time = std::filesystem::last_write_time(file);
+
+                // File creation
+                if (paths_.find(file.path().string()) == paths_.end()) // OR paths_.contains(file.path().string())
+                {
+                    paths_[file.path().string()] = current_file_last_write_time;
+                    action(file.path().string(), FileStatus::created);
+                }
+                // File modification
+                else
+                {
+                    if (paths_[file.path().string()] != current_file_last_write_time)
+                    {
+                        paths_[file.path().string()] = current_file_last_write_time;
+                        action(file.path().string(), FileStatus::modified);
+                    }
+                }
+            }
         }
     }
 
 private:
     std::unordered_map<std::string, std::filesystem::file_time_type> paths_;
+    bool running_ = true;
 };
